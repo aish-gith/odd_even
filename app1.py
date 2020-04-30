8305958d348b6ebd57b820e98d996a1fbfdc944d
aish-gith <aishwarya.ps@gmail.com>
2020-04-30 16:46:01 +0530

    homework2

---
+++ app1.py
@@ -0,0 +1,32 @@
+import calculator as c 
+
+x=float(input("enter first number:"))
+y=float(input("enter second number: "))
+
+'''z=c.add(x,y)
+print("addition result:",z)
+    
+w=c.sub(x,y)
+print("subtraction result:",w)
+    
+u=c.mul(x,y)
+print("multiplication result:",u)
+    
+v=c.div(x,y)
+print("division result:",v)'''
+
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
