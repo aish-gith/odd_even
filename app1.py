da4a8a547f12cf1f4925bae0cc4f0d67b5114b60
aish-gith <aishwarya.ps@gmail.com>
2020-04-30 19:18:57 +0530

    homework1

--- app1.py
+++ app1.py
@@ -1,22 +1,48 @@
 import calculator as c 
 
-x=float(input("enter first number:"))
-y=float(input("enter second number: "))
-
-'''z=c.add(x,y)
-print("addition result:",z)
-    
-w=c.sub(x,y)
-print("subtraction result:",w)
-    
-u=c.mul(x,y)
-print("multiplication result:",u)
-    
-v=c.div(x,y)
-print("division result:",v)'''
 
     
+loop = 1
+choice = 0
+while loop == 1:
+    print ("1) Addition")
+    print ("2) Subtraction")
+    print ("3) Multiplication")
+    print ("4) Division")
+    print ("5) Quit calculator")
+    choice=int(input("enter ur choice:"))
+    if choice == 1:
+        x=float(input("enter first number:"))
+        y=float(input("enter second number: "))
+
+        z=c.add(x,y)
+        print("addition result:",z)
         
+    elif choice == 2:
+        x=float(input("enter first number:"))
+        y=float(input("enter second number: "))
+        w=c.sub(x,y)
+        print("subtraction result:",w)
+
+       
+    elif choice == 3:
+        x=float(input("enter first number:"))
+        y=float(input("enter second number: "))
+        u=c.mul(x,y)
+        print("multiplication result:",u)
+    
+
+        
+    elif choice == 4:
+        x=float(input("enter first number:"))
+        y=float(input("enter second number: "))
+        v=c.div(x,y)
+        print("division result:",v)
+
+        
+    elif choice == 5:
+        loop = 0
+         
 
 
 
